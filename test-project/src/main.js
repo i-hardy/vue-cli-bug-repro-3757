085533abd2bdeaf09627e484ff9efd88e9a82390
Imogen Hardy <imogen.hardy@madgex.com>
2019-04-08 10:27:18 +0100

    Repro for vue-cli issue

---
+++ test-project/src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueRx from 'vue-rx';
+
+Vue.use(VueRx);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
